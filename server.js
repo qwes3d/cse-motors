368cd0ad58ad51a61c9dec0f5438d2785d1a97b1
qwes3d <uratation@gmail.com>
2025-07-14 11:35:07 +0000

    route updated

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ app.get('/contact', (req, res) => {
   res.send('Contact Page (Coming Soon)');
 });
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 
 /* ***********************
 * Express Error Handler
